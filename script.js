7c555c79ab7589081370ea50d63acfaf4441cadb
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-02-10 15:07:33 +0530

    make another version

--- script.js
+++ script.js
@@ -1,57 +1,8 @@
-// function to play sound when key is pressed also add border
-document.addEventListener("keydown", function (event) {
-    let code = event.keyCode;
-    switch (code) {
-        case 72:
-            focusedElement("home");
-            break;
-        case 65:
-            focusedElement("about");
-            break;
-        case 80:
-            focusedElement("project");
-            break;
-        case 66:
-            focusedElement("blog");
-            break;
-        case 67:
-            focusedElement("contact");
-            break;
-        // case 9:
-        //     tabPressed();
-        //     break;
-    }
-});
-
-
-// function to add border & play sound on selected id
-function focusedElement(selected_Id) {
-    let working_ID = selected_Id;
-    let play_ID = selected_Id.concat("1");
-    const allId = ["home", "about", "project", "blog", "contact"];
-
-    document.querySelectorAll("span").forEach(span => {
-        span.classList.remove("border");
-        // if (allId.includes(span.id) && span.classList.contains("focus-visible")) {
-        //     span.classList.remove("focus-visible");
-        // }
-    });
-
-    const selected = document.querySelector(`#${working_ID}`);
-    if (allId.includes(working_ID) && selected) {
-        selected.classList.add("border");
-        document.querySelector(`#${play_ID}`).play();
-    }
-}
-
-
-
 // function that write text in typing effect and also remove it letter by letter
 const text = "Code With Pankaj";
 let index = 0;
 let direction = 1;
 const textEl = document.querySelector("#text");
-
 function type() {
     if (direction === 1) {
         if (index < text.length) {
@@ -74,14 +25,96 @@ function type() {
     }
 }
 
-
 window.onload = type;
 
-//function to perform certain tasks when tab is pressed
-// function tabPressed() {
-//     document.querySelectorAll("span").forEach(span => span.classList.remove("border"));
-// }
+//function to trap focus inside my web page
+window.addEventListener("keydown", function (event) {
+    if (event.key === "Tab") {
+        event.preventDefault();
+        const focusableElements = Array.from(document.querySelectorAll("a[href], button, textarea, input[type='text'], input[type='radio'], input[type='checkbox'], select"));
+        const firstFocusableElement = focusableElements[0];
+        const lastFocusableElement = focusableElements[focusableElements.length - 1];
+        if (event.shiftKey) {
+            if (document.activeElement === firstFocusableElement) {
+                lastFocusableElement.focus();
+            } else {
+                let index = focusableElements.indexOf(document.activeElement);
+                focusableElements[index - 1].focus();
+            }
+        } else {
+            if (document.activeElement === lastFocusableElement) {
+                firstFocusableElement.focus();
+            } else {
+                let index = focusableElements.indexOf(document.activeElement);
+                focusableElements[index + 1].focus();
+            }
+        }
+    }
+});
+
+// function to play audio files related to button, on focus
+const buttons = document.querySelectorAll("button");
+const audioFiles = [
+    "./Audio_Files/Home.mp3",
+    "./Audio_Files/About.mp3",
+    "./Audio_Files/Projects.mp3",
+    "./Audio_Files/Blog.mp3",
+    "./Audio_Files/Contact.mp3"
+];
+let currentAudio = null;
+
+buttons.forEach((button, index) => {
+    button.addEventListener("focus", () => {
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        currentAudio = new Audio(audioFiles[index]);
+        currentAudio.play();
+    });
+
+    button.addEventListener("click", () => {
+
+        button.classList.add("button-outline");
+
+        if (!currentAudio || currentAudio.paused) {
+            currentAudio = new Audio(audioFiles[index]);
+            currentAudio.play();
+        }
+    });
+
+    button.addEventListener("blur", function () {
+        button.classList.remove("button-outline");
+    });
 
 
+    //set tooltip to buttons
+    tippy(button, {
+        content: `Tooltip = ${button.innerHTML.charAt(0)}`
+    });
 
-// TODO : Add feature - if tabindex outline is available then remove border & if border available then remove outline of tabindex
+    button.setAttribute("tabindex", "0");
+});
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        const focusedButton = document.activeElement;
+
+        if (focusedButton.tagName === "BUTTON") {
+            if (!currentAudio || currentAudio.paused) {
+                const index = Array.from(buttons).indexOf(focusedButton);
+                currentAudio = new Audio(audioFiles[index]);
+                currentAudio.play();
+            }
+        }
+    } else if (event.key.length === 1) {
+        const button = Array.from(buttons).find(
+            button => button.accessKey === event.key.toLowerCase()
+        );
+
+        if (button) {
+            button.focus();
+        }
+    }
+});
